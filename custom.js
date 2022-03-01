dbd29497921ee33ff7207f7497c3063a6893d75c
slightlybae <beaskytasia@yahoo.com>
2022-03-01 16:16:37 +0100

    new

--- custom.js
+++ custom.js
@@ -6,3 +6,19 @@ myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
 
+// function openAcct(evt, _acctinfo) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
